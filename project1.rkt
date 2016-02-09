4dc31e37b11d193be7aa4fee959edf0a92481237
Hun Jae Lee <hunjael@gmail.com>
2016-02-09 15:48:51 -0500

    added return method with proper commenting style need to check if commenting style is ok?

---
+++ project1.rkt
@@ -0,0 +1,22 @@
+;;; return statement ;;;
+;;; returns the given value or the evaluated value of the given calculation.
+;;; @param 
+(define return
+  (lambda (value)
+    (cond
+      (((number? value) ; if input is a number, just return that
+       value
+       ))
+      ;((pair? (value) ; if input is a evaluation, run evaluation method?
+      (else
+       0
+       )               ; not sure how to handle other cases
+      )))
+
+#! tests for return
+(return 150)              ; returns 150
+(return 3 * 4)            ; returns 12 (evaluates 3 * 4)
+(return 'hello)           ; returns the string "hello"
+; assume x = 3, y = 5 assigned
+(return x + y)            ; returns 8
+!#
