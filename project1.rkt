d9d6789fabe01ff5878eaf108066c212c3d903b5
Hun Jae Lee <hunjael@gmail.com>
2016-02-09 20:41:18 -0500

    work added

--- project1.rkt
+++ project1.rkt
@@ -13,10 +13,42 @@
        )               ; not sure how to handle other cases
       )))
 
-#! tests for return
-(return 150)              ; returns 150
-(return 3 * 4)            ; returns 12 (evaluates 3 * 4)
-(return 'hello)           ; returns the string "hello"
-; assume x = 3, y = 5 assigned
-(return x + y)            ; returns 8
-!#
+;;; tests are disabled until functions are complete
+;#! tests for return
+;(return 150)              ; returns 150
+;(return 3 * 4)            ; returns 12 (evaluates 3 * 4)
+;(return 'hello)           ; returns the string "hello"
+;; assume x = 3, y = 5 assigned
+;(return x + y)            ; returns 8
+;!#
+
+;;; boolean operators ;;;
+;;; X <boolOper> Y, where <boolOper> -> &&, ||, ! (but ! is a different case)
+;;; X, Y: the expressions to be evaluated
+
+(define bool
+  (lambda (X boolOper Y)
+    (case boolOper
+      (('&&) (AND X Y))
+      (('||) (OR X Y))
+      ; (('!) (NOT X Y))
+      )))
+
+
+;;; comparison operators ;;;
+;;; X <comparisonOper> Y, where <comparisonOper> -> ==, !=, <, >, <=. >=
+;;; X, Y: the expressions to be evaluated
+(define compare
+  (lambda (X compareOper Y)
+    (case compareOper
+      (('==) (eq? X Y))
+      (('>=) (>= X Y))
+      (('<=) (<= X Y))
+      (('!=) !(eq? X Y))
+      (('>) (> X Y))
+      (('<) (< X Y))
+      ; do we need a default case here?
+      )))
+
+
+;;; 
