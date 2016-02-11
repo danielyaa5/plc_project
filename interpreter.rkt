499f2abf4a693bc154389510fcc03ae2acd7c34d
Daniel Yak <danielyaa5@gmail.com>
2016-02-10 23:54:34 -0500

    added some skeletoning to the file, specifically process-parse-tree function

--- interpreter.rkt
+++ interpreter.rkt
@@ -5,8 +5,22 @@
 ;interprets some code from a file
 (define interpret
   (lambda (filename)
-  	; Feed file into parser
-  	; Evaluate the parse tree returned by parser
-  	; Return the appropriate value
     (parser filename)
   ))
+
+(define process-parse-tree
+  (lambda (tree state)
+    (cond
+      ((eq? (identifier tree) 'var))
+      ((eq? (identifier tree) '=))
+      ((eq? (identifier tree) 'if))
+      ((eq? (identifier tree) 'return))
+      ((eq? (identifier tree) 'while))
+      (else (error "bad-identifier" (identifier tree))))))
+
+   
+;the empty state
+(define init-state `((() ())))
+
+;parse tree elements
+(define identifier caar)
