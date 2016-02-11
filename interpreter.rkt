db5d6d32587d50c69cd9644ab1b0e42c46eadbdf
Daniel Yak <danielyaa5@gmail.com>
2016-02-10 21:02:50 -0500

    added gitignore file, also trying to get the interpret function working

--- interpreter.rkt
+++ interpreter.rkt
@@ -2,9 +2,11 @@
 ; Hun Jae Lee, Daniel Yakobian, & Justin Wang
 (load "simpleParser.scm")
 
+;interprets some code from a file
 (define interpret
   (lambda (filename)
   	; Feed file into parser
   	; Evaluate the parse tree returned by parser
   	; Return the appropriate value
+    (parser filename)
   ))
