1c5a55415318884ad00d8ad222f638b8901064b7
Daniel Yak <danielyaa5@gmail.com>
2016-02-10 20:35:04 -0500

    added interpreter file which will be used to load the code file and initalize

---
+++ interpreter.rkt
@@ -0,0 +1,10 @@
+; EECS 345 Class Project 1
+; Hun Jae Lee, Daniel Yakobian, & Justin Wang
+(load "simpleParser.scm")
+
+(define interpret
+  (lambda (filename)
+  	; Feed file into parser
+  	; Evaluate the parse tree returned by parser
+  	; Return the appropriate value
+  ))
