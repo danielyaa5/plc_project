b12be045b21949e119d5ad9c8e5ba0974f7b9413
Hun Jae Lee <hunjael@gmail.com>
2016-03-26 22:05:36 -0400

    test suite for proj 3

---
+++ project3/testRunner.rkt
@@ -0,0 +1,24 @@
+(load "p3.rkt")
+
+
+(interpret "test2/test1.txt")
+(interpret "test2/test2.txt")
+(interpret "test2/test3.txt")
+(interpret "test2/test4.txt")
+; (interpret "test2/test5.txt") throws exception
+(interpret "test2/test6.txt")
+(interpret "test2/test7.txt")
+(interpret "test2/test8.txt")
+(interpret "test2/test9.txt")
+(interpret "test2/test10.txt")
+; (interpret "test2/test11.txt") throws exception
+; (interpret "test2/test12.txt") throws exception
+; (interpret "test2/test13.txt") throws exception
+(interpret "test2/test14.txt")
+(interpret "test2/test15.txt")
+(interpret "test2/test16.txt")
+(interpret "test2/test17.txt")
+(interpret "test2/test18.txt")
+; (interpret "test2/test19.txt") throws exception
+
+; (interpret "test2/test20.txt") challenge doesn't work
